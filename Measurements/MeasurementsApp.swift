31a350a38988d99404614f8ef0c9a810d658e503
Abderrahman Ajid <abderrahman.ajid@gmail.com>
2022-04-08 09:16:55 +0000

    Initial Commit

---
+++ Measurements/MeasurementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeasurementsApp.swift
+//  Measurements
+//
+//  Created by Abderrahman Ajid on 8/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct MeasurementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
